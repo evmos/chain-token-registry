87d2f4e2b4a386050adfa50aeb069b49d19c6829
Julia Ortiz <94128293+julia-script@users.noreply.github.com>
2024-01-04 17:10:27 -0300

    Ensure token ibc.source matches chain identifiers (#86)

--- validate-tokens.sh
+++ validate-tokens.sh
@@ -1,3 +1,4 @@
+echo "Validating tokens against schema"
 for file in $(find ./tokens -name '*.json'); do
     # Validate the file against the schema
     if ! ajv validate -s ./schema.token.json -d "$file" --spec=draft2020 --errors=text; then
@@ -15,11 +16,29 @@ for file in $(find ./tokens -name '*.json'); do
 done
 
 
-# Validate that there are no duplicate coinDenoms
+# # Validate that there are no duplicate coinDenoms
+echo "Validating that there are no duplicate coinDenoms"
 jq '.coinDenom' ./tokens/*.json | sort | uniq -c | while read count value
 do
   if [ "$count" -gt 1 ]; then
     echo "Duplicate found: $value"
+    exit 1
+  fi
+done
+
+
+echo "Validating ibc source is also registered and matches identifier"
+
+identifiers=$(jq -r '.configurations[].identifier' ./chainConfig/*.json)
+
+for file in $(find ./tokens -name '*.json'); do
+  ibcSource=$(jq -r '.ibc.source' "$file")
+  coinDenom=$(jq -r '.coinDenom' "$file")
+
+  if echo "$identifiers" | grep -q "^$ibcSource$"; then
+    echo "- $coinDenom ibc source is valid"
+  else
+    echo "No match chain found '$coinDenom' for ibc.source '$ibcSource' must match a chain identifier."
     exit 1
   fi
 done
